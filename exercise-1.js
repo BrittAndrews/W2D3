5d08e4f01a68594753d00ed52e47137f07d9db85
BRITTANY ANDREWS <brittanyandrewsnyc@gmail.com>
2016-03-02 16:00:50 -0500

    exercise-1 done

--- exercise-1.js
+++ exercise-1.js
@@ -2,21 +2,27 @@
 // Write a function called `sum` that takes two
 // parameters and returns the sum of those 2 numbers.
 
-
+var sum = function(num1, num2){
+	return num1 + num2;
+};
 
 
 // Q2
 // Write a function named `avg` that takes 3 parameters
 // and returns the average of those 3 numbers.
 
-
+var avg = function(numero1, numero2, numero3){
+	return (numero1 + numero2 + numero3)/3;
+}
 
 
 // Q3
 // Write a function called `getLength` that takes one
 // parameter (a string) and returns the length
 
-
+var getLength = function(parameter1) {
+	return parameter1.length;
+}
 
 
 // Q4
@@ -25,7 +31,9 @@
 // parameter is greater than the first. Otherwise
 // the function should return `false`.
 
-
+var greaterThan = function(param1, param2) {
+	return param2 > param1;
+}
 
 
 // Q5
@@ -33,7 +41,9 @@
 // single parameter and returns a string that
 // is formated like "Hello, Name!" where *Name*
 // is the parameter that was passed in.
-
+var greet = function(Name){
+	return 'Hello, ' + Name;
+}
 
 
 
@@ -47,3 +57,12 @@
 // strings. For example:
 // words: "quick", "fox", "fence"
 // sentence: "quick brown fox jumps over the fence"
+
+
+var madlib = function(pm1, pm2, pm3, pm4) {
+	return pm1+ ' is a '+pm2+' sentence '+pm3+' I made ' + pm4;
+}
+
+
+
+
